2bca3145c6931fd095bfaf5da2feb848d87932a8
Hialii <110984478+Hialii@users.noreply.github.com>
2023-02-19 22:13:11 -0300

    start

--- javascript/game.js
+++ javascript/game.js
@@ -10,12 +10,35 @@ const characters = [
    'summer',
 ];
 
+let firstCard = '', secondCard = '';
 
 const createElement = (tag,className) => {
    const element = document.createElement(tag);
    element.className = className;
    return element;
 }
+
+const checkCards = () => {
+   
+}
+
+const revealCard = ({target}) => {  //target = elemento que foi clicado
+  if(target.parentNode.className.includes('reveal-card')) {
+   return;
+  }
+
+  if(firstCard === '') {
+   target.parentNode.classList.add("reveal-card") // pega todo o elemento "card" e adiciona class
+   firstCard = target.parentNode;
+  }else if(secondCard === '') {
+   target.parentNode.classList.add("reveal-card") 
+   secondCard = target.parentNode;
+
+   checkCards();
+  }
+}
+  
+   
 
 
 const createCard = (character) => {
@@ -27,18 +50,24 @@ const createCard = (character) => {
 
    card.appendChild(front);
    card.appendChild(back);
+   card.addEventListener('click', revealCard)
    grid.appendChild(card);
+
+   
 
    return card
 }
 
 const loadGame = () => {
+   
+   const duplicateCharacters = [...characters, ...characters];//spread operator ... espalhar os elementos de um array dentro de outro
+   
+   const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5); //os números menor que 0.5 serão retornados como negativos 
 
-   const duplicateCharacter = [ ... characters, ... characters ] //spread operator ... espalhar os elementos de um array dentro de outro
-   characters.forEach((character) => {
+   duplicateCharacters.forEach((character) => {
       const card = createCard(character);
       grid.appendChild(card)
    })
 }
 
-loadGame()
+loadGame();
